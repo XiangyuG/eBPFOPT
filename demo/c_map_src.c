911aceb4efa31bb6add0c7eee66cc39fe1d2203f
xiang95 <xiang95@node0.ebpfagent.meshbench-pg0.apt.emulab.net>
2026-05-29 11:02:55 -0600

    Add pure C map equivalence demo

---
+++ demo/c_map_src.c
@@ -0,0 +1,24 @@
+typedef unsigned int u32;
+
+#define MAP_SIZE 4
+
+struct int_map {
+  int present[MAP_SIZE];
+  u32 values[MAP_SIZE];
+};
+
+int map_prog(struct int_map *map, u32 key) {
+  if (key >= MAP_SIZE) {
+    return -1;
+  }
+
+  if (!map->present[key]) {
+    return 0;
+  }
+
+  u32 old_value = map->values[key];
+  u32 new_value = old_value + 7;
+  map->values[key] = new_value;
+
+  return map->values[key];
+}
---
+++ demo/c_map_tgt.c
@@ -0,0 +1,17 @@
+typedef unsigned int u32;
+
+#define MAP_SIZE 4
+
+struct int_map {
+  int present[MAP_SIZE];
+  u32 values[MAP_SIZE];
+};
+
+int map_prog(struct int_map *map, u32 key) {
+  if (key >= MAP_SIZE || !map->present[key]) {
+    return key >= MAP_SIZE ? -1 : 0;
+  }
+
+  map->values[key] += 7;
+  return map->values[key];
+}
